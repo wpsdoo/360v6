fadc82a773865cfc6a657fbdf8c413214cf23563
wpsdoo <104337859+wpsdoo@users.noreply.github.com>
2025-03-12 21:07:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,10 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git package https://github.com/sirpdboy/sirpdboy-package.git' >>feeds.conf.default
-
-
-
 #add openclash
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 
